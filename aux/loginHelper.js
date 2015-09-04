ba7c0e72bf7706f5f6f10ba288bc5331bb9112fd
Luca Gobbi <lucavgobbi@gmail.com>
2015-09-04 16:25:07 -0400

    -Fix on loginHelper

--- aux/loginHelper.js
+++ aux/loginHelper.js
@@ -11,30 +11,40 @@ function LoginHelper() {
 
 LoginHelper.prototype.validateToken = function (req, res, next) {
     var token = req.query.token;
-    User.findOne({ token: token }, function (err, user) {
-        if (err) {
-            res.status(500).json({error: true, type: 'internal_error', details: err});
-        } if (user == null) {
-            res.status(403).json({error: true, type: 'invalid_credentials', details: null});
-        } else {
-            req.loggedUser = user;
-            next();
-        }
-    });
+    if (token == null) {
+        res.status(400).json({ error: true, type: 'token_required', details: 'token was not provided'});
+    } else {
+        User.findOne({ token: token }, function (err, user) {
+            if (err) {
+                res.status(500).json({ error: true, type: 'internal_error', details: err });
+            }
+            if (user == null) {
+                res.status(403).json({ error: true, type: 'invalid_credentials', details: null });
+            } else {
+                req.loggedUser = user;
+                next();
+            }
+        });
+    }
 };
 
 LoginHelper.prototype.validateAdminToken = function (req, res, next) {
     var token = req.query.token;
-    User.findOne({ token: token, isAdmin: true }, function (err, user) {
-        if (err) {
-            res.status(500).json({error: true, type: 'internal_error', details: err});
-        } if (user == null) {
-            res.status(403).json({error: true, type: 'invalid_credentials', details: null});
-        } else {
-            req.loggedUser = user;
-            next();
-        }
-    });
+    if (token == null) {
+        res.status(400).json({ error: true, type: 'token_required', details: 'token was not provided'});
+    } else {
+        User.findOne({ token: token, isAdmin: true }, function (err, user) {
+            if (err) {
+                res.status(500).json({ error: true, type: 'internal_error', details: err });
+            }
+            if (user == null) {
+                res.status(403).json({ error: true, type: 'invalid_credentials', details: null });
+            } else {
+                req.loggedUser = user;
+                next();
+            }
+        });
+    }
 };
 
 
